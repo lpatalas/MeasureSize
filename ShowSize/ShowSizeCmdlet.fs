b22cc3a3009c0bf961903f7df23c29283f2c5639
Lukasz Patalas <lpatalas@gmail.com>
2020-09-12 02:01:37 +0200

    Allow to specify path explicitly

--- ShowSize/ShowSizeCmdlet.fs
+++ ShowSize/ShowSizeCmdlet.fs
@@ -12,10 +12,31 @@ type ItemSize(item: FileSystemInfo, size: int64) =
 type ShowSizeCmdlet() =
     inherit PSCmdlet()
 
-    [<Parameter(Mandatory = true, ValueFromPipeline = true)>]
+    [<Parameter(Mandatory = true, ParameterSetName = "Input", ValueFromPipeline = true)>]
     member val InputObject: obj = null with get, set
 
+    [<Parameter(Mandatory = true, ParameterSetName = "Path", Position = 0)>]
+    member val Path: string = "" with get, set
+
     override this.ProcessRecord() = 
-        let fileSystemInfo = FileSystem.getFileSystemInfo this.InputObject
-        let size = FileSystem.calculateSize fileSystemInfo
-        this.WriteObject(ItemSize(fileSystemInfo, size))
+        if this.ParameterSetName = "Input" then
+            let fileSystemInfo = FileSystem.getFileSystemInfo this.InputObject
+            let size = FileSystem.calculateSize fileSystemInfo
+            this.WriteObject(ItemSize(fileSystemInfo, size))
+        else
+            let (resolvedPaths, _) = this.SessionState.Path.GetResolvedProviderPathFromPSPath(this.Path)
+
+            let items =
+                if resolvedPaths.Count = 1 then
+                    let itemInfo = FileSystem.getFileSystemInfoForPath resolvedPaths.[0]
+                    match itemInfo with
+                    | :? FileInfo as fileInfo -> seq { fileInfo :> FileSystemInfo }
+                    | :? DirectoryInfo as directoryInfo -> directoryInfo.EnumerateFileSystemInfos()
+                    | _ -> invalidOp "FileSystemInfo is neither FileInfo nor DirectoryInfo"
+                else
+                    resolvedPaths
+                    |> Seq.map FileSystem.getFileSystemInfoForPath
+
+            items
+            |> Seq.map (fun info -> ItemSize(info, FileSystem.calculateSize info))
+            |> Seq.iter this.WriteObject
